70a07bdfdae46cad889721d9631bb009b3758ea4
tuesdaybloom <140499790+tuesdaybloom@users.noreply.github.com>
2024-07-01 17:04:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -9,50 +9,48 @@ import React from "react";
 
 import About from './components/About';
 function App() {
-  const [mode,setMode]=useState('light');
-  const [Tmode,setTMode]=useState('dark');
-  const[able,setAble]=useState('Enable');
-  const [alert,setalert]=useState(null);
-  const showAlert=(message,type)=>{
-     setalert({
-      msg:message,
-      type:type
-     })
-     setTimeout(()=>{
-        setalert(null)
-     },1500)
+  const [mode, setMode] = useState('light');
+  const [Tmode, setTMode] = useState('dark');
+  const [able, setAble] = useState('Enable');
+  const [alert, setalert] = useState(null);
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setalert(null)
+    }, 1500)
   }
-  const changeMode=()=>{
-    
-    if(mode==='light'){
+  const changeMode = () => {
+
+    if (mode === 'light') {
       setMode('dark')
       setAble('Disable')
       setTMode('light')
-      document.body.style.backgroundColor='#0c2864ef'
-      showAlert("Dark mode is Enabled","success")
+      document.body.style.backgroundColor = '#0c2864ef'
+      showAlert("Dark mode is Enabled", "success")
     }
-    else{
+    else {
       setMode('light')
       setAble('Enable')
       setTMode('Dark')
-      document.body.style.backgroundColor='#bbb4e0ef'
-      showAlert("Light mode is Enabled","success")
+      document.body.style.backgroundColor = '#bbb4e0ef'
+      showAlert("Light mode is Enabled", "success")
     }
   }
   return (
     <>
-   
-   <Navbar title="TextTweak" about='About TextTweak' mode={mode} changeMode={changeMode} able={able}/>
-   <Alert alert={alert}/>
-   <div id='bgcolor' >
-   <TextForm mode={Tmode} />
-   <About mode={Tmode} emode={mode}/>   
-         
-    
-   </div>
-  
+
+      <Navbar title="TextTweak" about='About TextTweak' mode={mode} changeMode={changeMode} able={able} />
+      <Alert alert={alert} />
+      <div id='bgcolor' >
+        <TextForm mode={Tmode} />
+        <About mode={Tmode} emode={mode} />
+      </div>
+
     </>
-    
+
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,10 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/about">{props.title}</a>
+    <a className="navbar-brand" href="#">{props.title}</a>
     
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        
-      </ul>
+     
     </div>
     <div className="d-flex flex-md-row flex-column">
     <Theme cmode={props.mode}/>
